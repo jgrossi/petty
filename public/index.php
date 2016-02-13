d0c432c9c3d9e415d6bf634cc9bc3322f2b94b5f
Junior Grossi <juniorgro@gmail.com>
2016-02-13 15:20:10 -0200

    Change base project to Laravel

--- public/index.php
+++ public/index.php
@@ -1,46 +1,58 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Petty URL Shortener</title>
-    <link rel="stylesheet" href="./assets/styles.css">
-</head>
-<body>
-    <div class="container">
-        <div class="header text-center">
-            <h1>Petty URL Shortener</h1>
-            <p class="lead">
-                Your own URL Shortener <br>
-                <code>http://jgro.me</code>
-            </p>
-            <ul class="nav nav-tabs nav-main">
-                <li class="active"><a href="#">New URL</a></li>
-                <li><a href="#">Previous URLs</a></li>
-            </ul>
-        </div>
-        <div class="body text-center">
-            <form action="" method="GET">
-                <div class="form-group">
-                    <input type="email" class="form-control" id="url" placeholder="http://example.com">
-                </div>
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary btn-block btn-lg">
-                        Short it!
-                    </button>
-                </div>
-            </form>
-            <div class="well well-sm well-url">
-                <div class="input-group input-group-lg">
-                    <input type="text" class="form-control" value="http://jgro.me/Inx7">
-                    <span class="input-group-btn">
-                        <button class="btn btn-success" type="button">Copy!</button>
-                    </span>
-                </div>
-            </div>
-        </div>
-        <div class="footer text-center">
-            &copy; 2016 <a href="http://github.com/jgrossi/petty">Petty URL Shortener</a>
-        </div>
-    </div>
-</body>
-</html>
+<?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylorotwell@gmail.com>
+ */
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| our application. We just need to utilize it! We'll simply require it
+| into the script here so that we don't have to worry about manual
+| loading any of our classes later on. It feels nice to relax.
+|
+*/
+
+require __DIR__.'/../bootstrap/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+|
+| We need to illuminate PHP development, so let us turn on the lights.
+| This bootstraps the framework and gets it ready for use, then it
+| will load up this application so that we can run it and send
+| the responses back to the browser and delight our users.
+|
+*/
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request
+| through the kernel, and send the associated response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have prepared for them.
+|
+*/
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
