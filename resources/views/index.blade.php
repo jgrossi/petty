fdd3e1fe718a2c8222b781073d107df29c4ed7c4
Junior Grossi <juniorgro@gmail.com>
2016-02-13 16:35:51 -0200

    Visual changes for Laravel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>Petty URL Shortener</title>
-    <link rel="stylesheet" href="./assets/styles.css">
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
 <body>
     <div class="container">
@@ -15,7 +16,7 @@
             </p>
             <ul class="nav nav-tabs nav-main">
                 <li class="active"><a href="#">New URL</a></li>
-                <li><a href="#">Previous URLs</a></li>
+                <li><a href="#">Generated URLs</a></li>
             </ul>
         </div>
         <div class="body text-center">
@@ -42,5 +43,6 @@
             &copy; 2016 <a href="http://github.com/jgrossi/petty">Petty URL Shortener</a>
         </div>
     </div>
+    <script src="{{ asset('js/scripts.js') }}"></script>
 </body>
 </html>
