aeef63b3278ab94362dae2883dfa46c12ac9453b
Junior Grossi <juniorgro@gmail.com>
2016-02-13 18:12:09 -0200

    Add petty config with custom domain

---
+++ config/petty.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Petty Domain
+    |--------------------------------------------------------------------------
+    |
+    | This value determines the domain used for your shortener. Every short
+    | url generated will use this domain.
+    |
+    */
+
+    'domain' => env('PETTY_DOMAIN', 'http://example.com'),
+
+];
