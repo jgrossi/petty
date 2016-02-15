60c889fef395cfafa0577fcc26f45f133b78709d
Junior Grossi <juniorgro@gmail.com>
2016-02-15 20:09:22 -0200

    Assets support for Font-Awesome

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
 
     $('#input-original-url').focus();
+    
+    $('.nav-main li').each(function() {
+        var href = $(this).find('a').attr('href');
+        if (href == fullUrl) {
+            $('.nav-main li').removeClass('active');
+            $(this).addClass('active');
+        }
+    });
 
 });
