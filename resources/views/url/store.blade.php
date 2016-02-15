7b2a5d48333d02081b1ebb37fc2820a215f0d2a6
Junior Grossi <juniorgro@gmail.com>
2016-02-15 18:27:59 -0200

    Clean input field after url generation

--- resources/views/url/store.blade.php
+++ resources/views/url/store.blade.php
@@ -1,6 +1,7 @@
 $('.form-group-url').removeClass('has-error')
     .find('.alert')
     .fadeOut();
+$('#input-original-url').val('');
 $('#input-url').val('{{ $shortUrl }}').select();
 
 {{--$('#btn-copy-url').focus();--}}
